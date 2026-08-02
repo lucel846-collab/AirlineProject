7a42ea314e5597723eb6911c224275db9f9dbfb5
Toshiya Yashiro <lucel846@gmail.com>
2026-08-02 15:07:46 +0900

    feat: normalize追加

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from src.error_report import export_validation_errors
 from src.master_data import (
     read_airport_master,
     read_airline_master,
-    read_route_master
+    read_route_master,
+    load_route_alias_dict,
+    load_airport_alias_dict,
 )
 from src.validator import validate
 from src.paths import (
@@ -12,28 +14,36 @@ from src.paths import (
     OUTPUT_FILE,
     ERROR_FILE,
 )
+from src.normalize import normalize
+from src.logger import logger
+
 
 
 def main():
 
+    logger.info("変換開始")
     df = read_excel(INPUT_FILE)
+    logger.info("マスタファイルを読み込みます。")
     airline_master = read_airline_master()
     airport_master = read_airport_master()
     route_master = read_route_master()
+    route_alias_dict = load_route_alias_dict()
+    airport_alias_dict = load_airport_alias_dict()
 
-    errors = validate(df, airport_master, airline_master, route_master)
+    logger.info("ファイルチェック開始")
+    errors = validate(df, airport_master, airline_master, route_master, route_alias_dict, airport_alias_dict)
     if errors:
 
         export_validation_errors(errors, ERROR_FILE)
 
-        print(f"{len(errors)}件のエラーがあります。")
-        print("ValidationError.csv を確認してください。")
+        logger.error(f"{len(errors)}件のエラーがあります。")
+        logger.info("ValidationError.csv を確認してください。")
 
         return
-
+    logger.info("CSV出力開始")
+    normalize(df, route_alias_dict, airport_alias_dict)
     export_csv(df, OUTPUT_FILE)
-
-    print("変換完了")
+    logger.info("変換完了")
 
 
 if __name__ == "__main__":
--- src/master_data.py
+++ src/master_data.py
@@ -4,6 +4,8 @@ from src.paths import (
     AIRPORT_MASTER_FILE,
     AIRLINE_MASTER_FILE,
     ROUTE_MASTER_FILE,
+    ROUTE_ALIAS_FILE,
+    AIRPORT_ALIAS_FILE,
 )
 
 def read_airport_master() -> pd.DataFrame:
@@ -14,3 +16,32 @@ def read_airline_master() -> pd.DataFrame:
 
 def read_route_master() -> pd.DataFrame:
     return pd.read_csv(ROUTE_MASTER_FILE)
+
+def read_route_alias_master() -> pd.DataFrame:
+    return pd.read_csv(ROUTE_ALIAS_FILE)
+
+def read_airport_alias_master() -> pd.DataFrame:
+    return pd.read_csv(AIRPORT_ALIAS_FILE)
+
+def create_route_alias_dict(route_alias_df: pd.DataFrame) -> dict[tuple[str, str, str], str]:
+    return {
+        (
+            row["AirlineCD"],
+            row["AirportOffice"],
+            row["RouteAlias"]
+        ): row["RouteCD"]
+        for _, row in route_alias_df.iterrows()
+    }
+def create_airport_alias_dict(airport_alias_df: pd.DataFrame) -> dict[str, str]:
+    return {
+        row["AirportAlias"]: row["AirportCD"]
+        for _, row in airport_alias_df.iterrows()
+    } 
+
+def load_route_alias_dict() -> dict[tuple[str, str, str], str]:
+    route_alias_df = read_route_alias_master()
+    return create_route_alias_dict(route_alias_df)
+
+def load_airport_alias_dict() -> dict[str, str]:
+    airport_alias_df = read_airport_alias_master()
+    return create_airport_alias_dict(airport_alias_df)
