2d0343b4c88315556b01662f3d4c33987b596985
Toshiya Yashiro <lucel846@gmail.com>
2026-08-05 08:28:56 +0900

    feat:required check add

--- src/normalize2.py
+++ src/normalize2.py
@@ -4,6 +4,10 @@ from src.master_data import MasterData
 
 
 def normalize2(df: pd.DataFrame,master: MasterData)   -> None:
+    logger.info("航空会社名追加開始")
+    df["航空会社名"] = df["航空会社"].map(master.get_airline_name)
+    logger.info("航空会社名追加完了")
+
     logger.info("ルートコード追加開始")
     # 航空会社コード、事業所、路線コードを結合してルートCDに変換
     df["路線CD"] = df.apply(
@@ -14,8 +18,9 @@ def normalize2(df: pd.DataFrame,master: MasterData)   -> None:
         ),
         axis=1
     )
+
     logger.info("ルートコード追加完了")
     logger.info("ルート名追加開始")
     df["路線名"] = df["路線CD"].map(master.get_route_name)
     logger.info("ルート名追加完了")
- 
+    
--- src/validator.py
+++ src/validator.py
@@ -1,11 +1,18 @@
 import pandas as pd
+
 from src.logger import logger
 from src.master_data import MasterData
+
 from src.validators.airline import validate_airline_code
 from src.validators.airport import validate_airport_alias
 from src.validators.date import validate_previous_date_check
 from src.validators.flight import validate_cancelled_flights
-from src.validators.required import validate_columns, validate_required
+from src.validators.required import (
+    validate_airport_office,
+    validate_columns,
+    validate_numeric,
+    validate_required,
+)
 from src.validators.route import validate_route_alias
 
 
@@ -21,6 +28,8 @@ def validate(df: pd.DataFrame,
     validate_airline_code(df, master, errors)
     validate_airport_alias(df, master, errors)
     validate_route_alias(df, master, errors)
+    validate_numeric(df, errors)
+    validate_airport_office(df, errors)
     logger.info("ファイルチェック完了")
     return errors   
 
