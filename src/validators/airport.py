eb14f5385d69d6a0dbbff3fca7e048309b528a14
Toshiya Yashiro <lucel846@gmail.com>
2026-08-04 22:19:22 +0900

    feat 到着予定空港チェック追加

--- src/validators/airport.py
+++ src/validators/airport.py
@@ -4,9 +4,9 @@ from src.validators.adderror import add_error
 
 
 def validate_airport_alias(df: pd.DataFrame, master: MasterData, errors: list[dict[str, any]]) -> None:
-    for column in ("出発空港", "到着空港"):
+    for column in ("出発空港", "到着空港", "到着予定空港"):
         for index, value in df[column].items():
-            if not master.exists_airport_alias(value):
+            if not pd.isna(value) and value != "" and not master.exists_airport_alias(value):
                 add_error(
                     errors,
                     index,
