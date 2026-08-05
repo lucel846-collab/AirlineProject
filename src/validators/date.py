cae1688e7efc21d4ddafcaafcff0f879ca082f6e
Toshiya Yashiro <lucel846@gmail.com>
2026-08-06 02:53:35 +0900

    feat:validator_result bug fix

--- src/validators/date.py
+++ src/validators/date.py
@@ -2,17 +2,16 @@ import pandas as pd
 from src.validator_result import ValidationResult
 
 
-def validate_previous_date_check(df: pd.DataFrame, errors:list[dict[str, any]]) -> None:
+def validate_previous_date_check(df: pd.DataFrame, result: ValidationResult) -> None:
     today = pd.Timestamp.today().normalize()
     first_date = today.replace(day=1)
     last_date_prev_month = first_date - pd.Timedelta(days=1)
     first_date_prev_month = last_date_prev_month.replace(day=1)
     for index,row in df.iterrows():
         if (row["運航日"] > last_date_prev_month or row["運航日"] < first_date_prev_month):
-            ValidationResult.add_error(
-                errors,
-                index,
-                "運航日",
-                row["運航日"],
-                "運航日は前月である必要があります。"
+            result.add_error(
+                index=index,
+                column="運航日",
+                value=row["運航日"],
+                message="運航日は前月である必要があります。"
             )
--- src/validators/route.py
+++ src/validators/route.py
@@ -3,7 +3,7 @@ from src.master_data import MasterData
 from src.validator_result import ValidationResult
 
 
-def validate_route_alias(df: pd.DataFrame, master: MasterData, errors: list[dict[str, any]]) -> None:
+def validate_route_alias(df: pd.DataFrame, master: MasterData,result: ValidationResult) -> None:
 
     for index, row in df.iterrows():
         key = (
@@ -12,11 +12,10 @@ def validate_route_alias(df: pd.DataFrame, master: MasterData, errors: list[dict
             f"{row['出発空港']}{row['到着空港']}"
         )
         if not master.exists_route_alias(*key):
-            ValidationResult.add_error(
-                errors,
-                index,
-                "路線コード",
-                f"{row['航空会社']}-{row['事業所']}-{row['出発空港']}{row['到着空港']}",
-                "路線コードがエイリアスマスタに存在しません"
+            result.add_error(
+                index=index,
+                column="路線コード",
+                value=f"{row['航空会社']}-{row['事業所']}-{row['出発空港']}{row['到着空港']}",
+                message="路線コードがエイリアスマスタに存在しません"
             )
 
